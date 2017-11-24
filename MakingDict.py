7883e2a543519f46908eed0abb7ccb8460e44f6a
Jared <jaredjkim@gmail.com>
2017-11-23 23:40:13 -0800

    first commit

---
+++ MakingDict.py
@@ -0,0 +1,7 @@
+name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
+favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
+
+
+myfavanimal = zip(name, favorite_animal)
+myfavanimal_dict = dict(myfavanimal)
+print myfavanimal_dict
